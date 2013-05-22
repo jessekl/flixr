3007b8ff487fdc8c9dd2f2cbb716041407137cd6
Muhammad Ahsan Ali <sn.ahsanali@gmail.com>
2013-05-22 23:22:28 +0500

    Adding Tornado server

---
+++ runtornado.py
@@ -0,0 +1,10 @@
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+from fbone import create_app
+if __name__ == '__main__':
+	app = create_app()
+	app.debug = True
+	http_server = HTTPServer(WSGIContainer(app))
+	http_server.listen(5000)
+	IOLoop.instance().start()
