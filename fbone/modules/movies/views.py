a77a8cbb302285ce4844df7d156701689704c754
Michelle <mrojass1028@gmail.com>
2015-05-27 22:47:16 -0400

    scraping gets movie titles

--- fbone/modules/movies/views.py
+++ fbone/modules/movies/views.py
@@ -21,8 +21,9 @@ BASE_URL = "http://www.imdb.com/movies-coming-soon/"
 def list_upcoming():
     today = datetime.date.today()
     month = today.month
- 
-    for m in range(month, month + 7):
+    movies_dict = {}
+    
+    for m in range(month + 1, month + 7):
     	req = requests.get(BASE_URL + str(YEAR) + "-" + str(m).zfill(2))
     	data = req.text
     	s = BeautifulSoup(data)
@@ -30,7 +31,17 @@ def list_upcoming():
     		release_date = day.text.split()
     		release_month = release_date[0]
     		release_day = release_date[1].zfill(2)
-    		print release_month + " " + release_day
+    		release_date = release_month + " " + release_day
+    		movies = day.find_next_siblings()
+    		movies_list = []
+    		for movie in movies:
+    			if movie.name != "div":
+    				break
+    			table = movie.find("table")
+    			name = table.find("h4")
+    			movies_list.append(name.text.strip())
+    		movies_dict[release_date] = movies_list
+    		print movies_dict[release_date]
     	
-    return render_template("movies/index.html", movies=movies)
+    return render_template("movies/index.html", movies_dict=movies_dict)
      
